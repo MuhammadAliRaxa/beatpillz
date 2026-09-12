a18aafea98c02bb1cefa9e0686444dceb99ef2ab
Muhammad Ali Raza <muhammadalixxara@gmail.com>
2026-09-12 16:56:25 +0500

    Done user

--- app/Http/Resources/Mobile/UserResource.php
+++ app/Http/Resources/Mobile/UserResource.php
@@ -12,11 +12,21 @@ class UserResource extends JsonResource
             return [];
         }
 
+        // Safely parse address as an array whether it is stored as array, JSON string, or object
+        $address = [];
+        if (is_array($this->address)) {
+            $address = $this->address;
+        } elseif (is_string($this->address)) {
+            $address = json_decode($this->address, true) ?? [];
+        } elseif (is_object($this->address)) {
+            $address = (array) $this->address;
+        }
+
         return [
             'id'                  => $this->id,
             'firstname'           => $this->firstname,
             'lastname'            => $this->lastname,
-            'fullname'            => $this->getName(),
+            'fullname'            => method_exists($this->resource, 'getName') ? $this->getName() : trim(($this->firstname ?? '') . ' ' . ($this->lastname ?? '')),
             'username'            => $this->username,
             'email'               => $this->email,
             'avatar'              => $this->avatar ? asset($this->avatar) : null,
@@ -27,13 +37,13 @@ class UserResource extends JsonResource
             'is_featured_author'  => (bool) $this->is_featured_author,
             'exclusivity'         => $this->exclusivity,
             'address'             => [
-                'line_1'       => @$this->address->line_1 ?? '',
-                'line_2'       => @$this->address->line_2 ?? '',
-                'city'         => @$this->address->city ?? '',
-                'state'        => @$this->address->state ?? '',
-                'zip'          => @$this->address->zip ?? '',
-                'country'      => @$this->address->country ?? '',
-                'country_name' => $this->getCountry() ?? '',
+                'line_1'       => $address['line_1'] ?? $address['address_1'] ?? '',
+                'line_2'       => $address['line_2'] ?? $address['address_2'] ?? '',
+                'city'         => $address['city'] ?? '',
+                'state'        => $address['state'] ?? '',
+                'zip'          => $address['zip'] ?? $address['postal_code'] ?? '',
+                'country'      => $address['country'] ?? '',
+                'country_name' => method_exists($this->resource, 'getCountry') ? ($this->getCountry() ?? '') : ($address['country'] ?? ''),
             ],
             'balance'             => (float) $this->balance,
             'currency'            => (function_exists('defaultCurrency') && defaultCurrency()) ? defaultCurrency()->code : 'USD',
@@ -43,7 +53,7 @@ class UserResource extends JsonResource
             'total_reviews'       => (int) $this->total_reviews,
             'avg_reviews'         => (float) $this->avg_reviews,
             'total_followers'     => (int) $this->total_followers,
-            'social_links'        => $this->profile_social_links ?? [],
+            'social_links'        => is_string($this->profile_social_links) ? (json_decode($this->profile_social_links, true) ?? []) : ($this->profile_social_links ?? []),
             'created_at'          => $this->created_at ? $this->created_at->toISOString() : null,
         ];
     }
